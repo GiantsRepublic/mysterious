b8c4b061b9a3bebf6636c553bd222ff9237f4e38
kwei <kwei@uncc.edu>
2019-05-30 00:19:19 -0400

    Initial Commit

---
+++ Mysterious/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Mysterious
+//
+//  Created by Jason Wei on 5/30/19.
+//  Copyright © 2019 Kaifan Wei. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
